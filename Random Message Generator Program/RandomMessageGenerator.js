b72034cbaf40862ac4abb71cb7cb304bd4c61d8f
Asagohano <jganch@gmail.com>
2023-09-04 11:42:11 +0800

    Completed RandomMessageGenerator

--- Random Message Generator Program/RandomMessageGenerator.js
+++ Random Message Generator Program/RandomMessageGenerator.js
@@ -2,8 +2,19 @@
 const magicEightBallAnswers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
 
 const returnAnswer = () => {
+    console.log("    /\\ ")
+    console.log("   /__\\  ")
+    console.log(" <( o.o)> ")
+    console.log("You could say '" +  provideAnswer() + "'");
+    console.log("\n");
+    console.log("Or better yet '" +  provideAnswer()  + "'");
+    console.log("\n");    
+    console.log("But the answer definately is '" +  provideAnswer()  + "'");
+}
+
+const provideAnswer = () => {
     let answerID = Math.floor(Math.random() * 20);
-    console.log(magicEightBallAnswers[answerID]);
+    return magicEightBallAnswers[answerID];
 }
 
 returnAnswer();
